55ac1cb542579eedf31bc858fbe2d4db74ad3681
Muqadas Ilyas <muqadasilyas2005@gmail.com>
2026-09-06 16:16:19 +0500

    fix: Fixed form issue

--- hms/patient/views.py
+++ hms/patient/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+from patient.forms import PatientForm
 from patient.models import Patient
 from django.shortcuts import get_object_or_404
 
@@ -16,19 +17,12 @@ def patient_detail(request, id):
 
 def create_patient(request):
     if request.method=='POST':
-        first_name=request.POST['first_name']
-        last_name=request.POST['last_name']
-        gender=request.POST['gender']
-        age=request.POST['age']
-        blood_type=request.POST['blood_type']
-        phone=request.POST['phone']
-        date_of_birth=request.POST['dob']
-        patient=Patient.objects.create(first_name=first_name,
-                                       last_name=last_name,
-                                       gender=gender,
-                                       dob=date_of_birth,
-                                       phone=phone,
-                                       blood_type=blood_type,
-                                       age=age)
-        return redirect('patient_detail',id=patient.id)
-    return render(request,'patient/patient_form.html')
+        form=PatientForm(request.POST)
+        if form.is_valid():
+            patient=form.save()
+            return redirect('patient_detail', id=patient.id)
+    else:
+        form = PatientForm()
+
+    return render(request,'patient/patient_form.html',
+                  {'form':form})
